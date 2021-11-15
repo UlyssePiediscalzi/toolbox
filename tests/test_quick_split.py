4fa66ebbab0c51ef66ba037331d05e9eade512ef
Ulysse Piediscalzi <upiedi@gmail.com>
2021-11-15 15:15:06 -0500

    debugging test

--- tests/test_quick_split.py
+++ tests/test_quick_split.py
@@ -1,6 +1,6 @@
-from toolbox.quick_split import quick_split
+from toolbox.quick_split import ml_split
 import numpy as np
 
 def test_quick_split():
     X, y = np.arange(10).reshape((5, 2)), range(5)
-    assert len(quick_split(X, y)) == 4
+    assert len(ml_split(X, y)) == 4
